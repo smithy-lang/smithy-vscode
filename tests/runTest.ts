35f402eb58bc0725f3aac7772794ed860fe40ac4
Chase Coalwell <chasecoa@amazon.com>
2022-05-17 10:14:09 -0600

    Add Selector commands (#33)

--- tests/runTest.ts
+++ tests/runTest.ts
@@ -1,17 +1,29 @@
-import * as path from "path";
+import { resolve } from "path";
 
 import { runTests } from "@vscode/test-electron";
 
 async function go() {
   try {
-    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
-    const extensionTestsPath = path.resolve(__dirname, "./suite");
-    const testWorkspace = path.resolve(__dirname, "../../test-fixture");
+    const extensionDevelopmentPath = resolve(__dirname, "../../");
+
+    // Suite 1 - Extension registration and launching language server
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath: resolve(__dirname, "./suite1"),
+      launchArgs: [resolve(__dirname, "../../test-fixtures/suite1")],
+    });
+
+    // Suite 2 - Diagnostics from broken model
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath: resolve(__dirname, "./suite2"),
+      launchArgs: [resolve(__dirname, "../../test-fixtures/suite2")],
+    });
 
     await runTests({
       extensionDevelopmentPath,
-      extensionTestsPath,
-      launchArgs: [testWorkspace],
+      extensionTestsPath: resolve(__dirname, "./suite3"),
+      launchArgs: [resolve(__dirname, "../../test-fixtures/suite3")],
     });
   } catch (err) {
     console.error("Failed to run tests");
