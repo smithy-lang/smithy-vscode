f3a8f68169a5dd4769dea18a2ca3172717170a0d
Chase Coalwell <782571+srchase@users.noreply.github.com>
2023-03-16 09:48:31 -0600

    Register formatter (#67)
    * Register formatter

---
+++ tests/suite5/extension.test.ts
@@ -0,0 +1,24 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import { getDocUri, waitForServerStartup } from "../helper";
+
+suite("formatting tests", () => {
+  test("Should register Smithy formatter", async () => {
+    const smithyMainUri = getDocUri("suite5/smithy/main.smithy");
+    const doc = await vscode.workspace.openTextDocument(smithyMainUri);
+    await vscode.window.showTextDocument(doc);
+    await waitForServerStartup();
+    const edits: vscode.TextEdit[] = await vscode.commands.executeCommand(
+      "vscode.executeFormatDocumentProvider",
+      smithyMainUri
+    );
+    const edit: vscode.TextEdit = edits[0];
+    const expectedRange = new vscode.Range(new vscode.Position(12, 0), new vscode.Position(12, 0));
+    const range = edit.range;
+    const newText = edit.newText;
+
+    assert.equal(edits.length, 1);
+    assert.equal(range.isEqual(expectedRange), true);
+    assert.equal(newText, "    ");
+  }).timeout(10000);
+});
